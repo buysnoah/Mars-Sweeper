7f010f6aa9964d5d473a058f148d25385dba0fbe
buysnoah <104743939+buysnoah@users.noreply.github.com>
2022-09-03 00:47:27 +0200

    Added overlay and tile selection (almost)
    Added the overlay and finished up the mouse hovering, tile selection is easily implementable now, but I thought I'd leave it for next commit as that will include the actual game generation (wishful thinking)

--- main.lua
+++ main.lua
@@ -1,9 +1,30 @@
 love.graphics.setDefaultFilter("nearest", "nearest")
 local tiles = {}
+--[[TILES:
+    1=Covered
+    2=Uncovered
+    3=Ice
+    4=Junk
+    5=One
+    6=Two
+    7=Three
+    8=Four
+    9=Five
+    10=Six
+    11=Seven
+    12=Eight
+    13=Cursor
+]]
 local tilesheet = love.graphics.newImage("images/tiles.png")
+local overlay = love.graphics.newImage("images/overlay.png")
 local gridinfo = {sizex=21, sizey=15}
+local mouse = {}
+local tileselected = false
 
 function love.load()
+    local cursorimage = love.graphics.newImage("images/cursor.png") 
+    local cursor = love.mouse.newCursor("images/cursor.png", cursorimage:getWidth()/2, cursorimage:getHeight()/2)
+    love.mouse.setCursor(cursor)
     love.window.setTitle("Mars Sweeper")
     for y=0, tilesheet:getWidth()/16-1 do
         for x=0, tilesheet:getHeight()/16-1 do
@@ -21,24 +42,36 @@ function love.load()
     for y=0, gridinfo.sizey-1 do
         gridinfo.grid[y+1] = {}
         for x=0, gridinfo.sizex-1 do
-            --[[TODO:
-            Make a class for the tiles, so that I can detect clicks on them and assign them
-            properties and such.
-            ]] 
-            local spotinfo = {}
+            local spotinfo = {image=tiles[1], px=x*16+8, py=y*16+8, x=x+1, y=y+1, selected = false}
             table.insert(gridinfo.grid[y+1], x+1, spotinfo)
         end
     end
 end
 
 function love.update(dt)
+    mouse.x = math.ceil((love.mouse.getX()-16)/32)
+    mouse.y = math.ceil((love.mouse.getY()-16)/32)
+    for i,v in ipairs(gridinfo.grid) do
+        for j,y in ipairs(v) do
+            if mouse.y == i and mouse.x == j then
+                y.selected = true            
+            else
+                y.selected = false
+            end
+        end
+    end
 end
 
 function love.draw()
     love.graphics.applyTransform(love.math.newTransform():scale(2,2))
     for i,v in ipairs(gridinfo.grid) do
-        for j,_ in ipairs(v) do
-            love.graphics.draw(tilesheet, tiles[1], (j-1)*16+8, (i-1)*16+8)
+        for j,y in ipairs(v) do
+            love.graphics.draw(tilesheet, y.image, y.px, y.py)
+            if y.selected then
+                love.graphics.draw(tilesheet, tiles[13], y.px, y.py)
+            end
         end
     end
-end
+    love.graphics.draw(overlay)
+    --love.graphics.print("mouse ( x:"..mouse.x .." y:"..mouse.y ..")\nselectedtile ("..(((selectedtile=="NONE") and "NONE)") or "x:"..selectedtile.x.." y:"..selectedtile.y..")"))
+end 
